1d28c7a42100af8d05e36bae15267c1d0aa6e399
SneakyBrian <SneakyBrian@gmail.com>
2012-10-05 19:20:08 +0100

    Initial Version

---
+++ XML2JSON/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace XML2JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputXml = args[0];
+            var outputJson = args[1];
+
+            var doc = new XmlDocument();
+            doc.Load(inputXml);
+
+            //strip comments from xml
+            var comments = doc.SelectNodes("//comment()");
+
+            if (comments != null)
+            {
+                foreach (var node in comments.Cast<XmlNode>())
+                {
+                    if (node.ParentNode != null)
+                        node.ParentNode.RemoveChild(node);
+                }
+            }
+
+            var rawJsonText = JsonConvert.SerializeXmlNode(doc.DocumentElement, Formatting.Indented);
+
+            //strip the @ and # characters
+            var strippedJsonText = Regex.Replace(rawJsonText, "(?<=\")(@)(?!.*\":\\s )", string.Empty, RegexOptions.IgnoreCase);
+            strippedJsonText = Regex.Replace(strippedJsonText, "(?<=\")(#)(?!.*\":\\s )", string.Empty, RegexOptions.IgnoreCase);
+
+            //save out
+            using (var outputFileStream = File.Open(outputJson, FileMode.Create, FileAccess.Write))
+            {
+                using (var outputStreamWriter = new StreamWriter(outputFileStream))
+                {
+                    outputStreamWriter.Write(strippedJsonText);
+                    outputStreamWriter.Flush();
+                    outputStreamWriter.Close();
+                }
+            }
+        }
+    }
+}
