feb044b1361294ad12591d8fba74ddafc01f9ec5
SneakyBrian <SneakyBrian@users.noreply.github.com>
2014-03-19 21:36:29 +0000

    GET api working. POST api not.

--- XML2JSON.Core/Converter.cs
+++ XML2JSON.Core/Converter.cs
@@ -1,13 +1,22 @@
 ﻿using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Xml;
 using Newtonsoft.Json;
 using Formatting = Newtonsoft.Json.Formatting;
 
 namespace XML2JSON.Core
 {
+    /// <summary>
+    /// Class to handle xml -> json conversion
+    /// </summary>
     public static class Converter
     {
+        /// <summary>
+        /// converts xml string to json string
+        /// </summary>
+        /// <param name="xml">xml data as string</param>
+        /// <returns>json data as string</returns>
         public static string ConvertToJson(string xml)
         {
             var doc = new XmlDocument();
@@ -32,6 +41,16 @@ namespace XML2JSON.Core
             strippedJsonText = Regex.Replace(strippedJsonText, "(?<=\")(#)(?!.*\":\\s )", string.Empty, RegexOptions.IgnoreCase);
 
             return strippedJsonText;
+        }
+
+        /// <summary>
+        /// async version of conversion function
+        /// </summary>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static async Task<string> ConvertToJsonAsync(string xml)
+        {
+            return await Task<string>.Factory.StartNew(() => ConvertToJson(xml));
         }
     }
 }
---
+++ XML2JSON.Web.MVC/Controllers/ConvertController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using XML2JSON.Core;
+
+namespace XML2JSON.Web.MVC.Controllers
+{
+    public class ConvertController : ApiController
+    {
+        /// <summary>
+        /// Gets the xml data at the specified uri and converts it to json before returning it
+        /// </summary>
+        /// <param name="uri">uri of xml data</param>
+        /// <param name="callback">javascript callback for jsonp usage. optional</param>
+        /// <returns>json encoded data</returns>
+        public async Task<HttpResponseMessage> Get(string uri, string callback = null)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(uri);
+                var xml = await response.Content.ReadAsStringAsync();
+
+                var json = await Converter.ConvertToJsonAsync(xml);
+
+                string result;
+
+                if (string.IsNullOrWhiteSpace(callback))
+                {
+                    result = json;
+                }
+                else
+                {
+                    result = callback + "(" + json + ");";
+                }
+
+                return new HttpResponseMessage 
+                {
+                    Content = new StringContent(result)
+                };
+            }
+        }
+
+        /// <summary>
+        /// encodes the posted xml data in json format and returns it
+        /// </summary>
+        /// <param name="xml">xml data</param>
+        /// <param name="callback">javascript callback for jsonp usage. optiona</param>
+        /// <returns>json encoded data</returns>
+        public async Task<HttpResponseMessage> Post([FromBody]string xml, string callback = null)
+        {
+            var json = await Converter.ConvertToJsonAsync(xml);
+
+            string result;
+
+            if (string.IsNullOrWhiteSpace(callback))
+            {
+                result = json;
+            }
+            else
+            {
+                result = callback + "(" + json + ");";
+            }
+
+            return new HttpResponseMessage
+            {
+                Content = new StringContent(result)
+            };
+        }
+    }
+}
