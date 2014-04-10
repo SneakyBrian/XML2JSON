b7875ee45e60435e6bf8d959d51413ff6587cb21
SneakyBrian <SneakyBrian@users.noreply.github.com>
2014-04-11 00:16:22 +0100

    Got POST working - need to specify Content-Type: text/xml in request

--- XML2JSON.Web.MVC/Global.asax.cs
+++ XML2JSON.Web.MVC/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using XML2JSON.Web.MVC.Formatters;
 
 namespace XML2JSON.Web.MVC
 {
@@ -22,6 +23,8 @@ namespace XML2JSON.Web.MVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configuration.Formatters.Insert(0, new TextMediaTypeFormatter());
         }
     }
 }
